112d986502f03380243b481604671603b723dffd
AlexLarentis01 <42517191+AlexLarentis01@users.noreply.github.com>
2021-07-25 21:40:19 +0200

    cleanup

--- alpgrow_vernemq_plugin/test/alpgrow_vernemq_plugin_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AlpgrowVernemqPluginTest do
-  use ExUnit.Case
-  doctest AlpgrowVernemqPlugin
-
-  test "greets the world" do
-    assert AlpgrowVernemqPlugin.hello() == :world
-  end
-end
